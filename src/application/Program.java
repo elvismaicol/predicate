342cbbca59459d152a91737d6e289eda21ca720c
elvismaicol <elvismaicol@gmail.com>
2020-02-02 22:13:44 -0300

    2ª Parte - Implementão interface para imp. Predicate

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import entities.Product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import util.ProductPredicate;
 
 public class Program {
 
@@ -17,7 +18,7 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
         
-        list.removeIf(p -> p.getPrice() >= 100);
+        list.removeIf(new ProductPredicate());
         
         for (Product p : list) {
             System.out.println(p);
