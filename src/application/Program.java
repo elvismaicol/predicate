8da8a8abc2946eb2f1f8d58cd12af935ec495bf8
elvismaicol <elvismaicol@gmail.com>
2020-02-02 22:48:00 -0300

    5ª Parte - Expressão lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,7 @@ import entities.Product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import util.ProductPredicate;
+import java.util.function.Predicate;
 
 public class Program {
 
@@ -18,8 +18,11 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
         
-        //Reference method com método NÂO estático
-        list.removeIf(Product :: nonStaticProductPredicate);
+        double min = 100.0;
+        //Expressão lambda declarada
+        Predicate<Product> pred = p -> p.getPrice() >= min;
+        
+        list.removeIf(pred);
         
         for (Product p : list) {
             System.out.println(p);
